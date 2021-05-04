91b4ae2b5b9b1cfba244fba67e53bd93a3d6b576
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2021-05-04 14:37:00 -0700

    playground2

--- playground2.py
+++ playground2.py
@@ -9,6 +9,7 @@ import functools as ft
 from pathlib import Path
 import zarr
 from joblib import Parallel, delayed
+import dask as da
 import dask.array as daa
 import dask_ml.preprocessing as dmlp
 import dask_ml.decomposition as dmld
@@ -125,7 +126,8 @@ class Sparse(tfk.layers.Layer):
             return result
 
         outputs = tf.map_fn(lambda x: _mult(*x), self.kj, fn_output_signature=tf.float32)
-        outputs = tf.reshape(outputs, (tf.shape(inputs)[0], self.kj[0].shape[0]))
+        outputs = tf.reshape(outputs, (self.kj[0].shape[0], tf.shape(inputs)[0]))
+        outputs = tf.transpose(outputs)
         return outputs
 
 x4_1 = x1.sel(cols=x1['mean']>(-7))
@@ -143,42 +145,81 @@ x4_3 = x4_3.reset_index().reset_index().set_index('index').rename(columns={'leve
 x4_2 = x4_2.join(x4_3, how='inner')
 x4_2 = x4_2[['i', 'j']].reset_index(drop=True).sort_values(['i', 'j'])
 
-x5_3 = tfk.Sequential([
-    tfk.layers.InputLayer((x4_4.shape[1],)),
-    Sparse(np.array(x4_2)),
-    tfk.layers.Dense(x4_4.shape[1])
-])
-x5_3.compile(optimizer='adam', loss='mean_squared_error')
-x5_3.summary()
-
-def _x5_4():
+def _x4_5():
     for chunk in chunk_iter(x4_4.chunks[0]):
         x = x4_4[chunk,:].compute()
         for i in range(x.shape[0]):
             yield x[i,:]
+x4_5 = tf.data.Dataset.from_generator(_x4_5, output_types=x4_4.dtype, output_shapes=(x4_4.shape[1],)).\
+    map(lambda row: (row, row)).\
+    batch(1000).repeat()
 
-x5_4 = tf.data.Dataset.from_generator(
-    lambda: ((row, row) for row in _x5_4()),
-    output_types=(tf.float32, tf.float32),
-    output_shapes=((x4_4.shape[1],), (x4_4.shape[1],))
-)
-x5_4 = x5_4.batch(1000).repeat()
 
-x5_3.fit(x5_4, epochs=2, steps_per_epoch=12)
+x5_3 = tfk.Sequential([
+    tfk.layers.InputLayer((x4_4.shape[1],)),
+    #Sparse(np.array(x4_2)),
+    tfk.layers.Dense(x4_2.shape[0], use_bias=False),
+    tfk.layers.Dense(x4_4.shape[1])
+])
+x5_3.compile(optimizer='adam', loss='mse')
+x5_3.summary()
 
-(x5_3.predict(x4_4)**2).mean()
+x5_3.fit(x4_5, epochs=100, steps_per_epoch=12)
 
-(x5_4**2).mean()
+x5_5 = [
+    daa.from_delayed(
+        da.delayed(x5_3.predict)(x4_4[chunk,:]),
+        shape=(chunk.stop-chunk.start, x4_4.shape[1]),
+        dtype=x4_4.dtype
+    )
+    for chunk in chunk_iter(x4_4.chunks[0])
+]
+x5_5 = daa.concatenate(x5_5, axis=0)
+
+((x4_4-x5_5)**2).mean().compute()
+
+plt.scatter(x5_5[0,:], x4_4[0,:])
+
 
 x7_1 = set(x4_2.j)
 x7_2 = set(np.arange(x4_4.shape[1]))-x7_1
 x7_2 = x4_4[:,list(x7_2)]
 x7_1 = x4_4[:,list(x7_1)]
 x7_1 = x7_1.rechunk((x7_1.chunks[0], x7_1.shape[1]))
-x7_1 = x7_1[chunk_perm(x7_1.chunks[0]),:]
+#x7_1 = x7_1[chunk_perm(x7_1.chunks[0]),:]
 x7_3 = daa.linalg.qr(x7_1)
 x7_3 = x7_1 @ daa.linalg.solve(x7_3[1], x7_3[0].T @ x7_2)
-(x7_2**2).mean().compute()
-(x7_3**2).mean().compute()
-
 ((x7_2-x7_3)**2).mean().compute()
+
+
+
+
+
+import sklearn.preprocessing as sklp
+import sklearn.decomposition as skld
+
+x8_1 = np.random.randn(10000, 500)
+x8_1 = sklp.StandardScaler().fit_transform(x8_1)
+
+x8_4 = skld.PCA(n_components=100).fit(x8_1)
+x8_5 = x8_4.inverse_transform(x8_4.transform(x8_1))
+
+x8_2_dense = tfk.layers.Dense(100, use_bias=False)
+x8_2_sparse = Sparse(np.vstack([
+    np.stack([[i]*x8_1.shape[1], np.arange(x8_1.shape[1])], axis=1)
+    for i in range(x8_2_dense.units)
+]))
+
+x8_2 = tfk.Sequential([
+    tfk.layers.InputLayer((x8_1.shape[1],)),
+    x8_2_sparse,
+    tfk.layers.Dense(x8_1.shape[1], use_bias=False)
+])
+x8_2.compile(optimizer='adam', loss='mse')
+x8_2.fit(x8_1, x8_1, epochs=500, batch_size=100)
+x8_3 = x8_2.predict(x8_1)
+
+print(((x8_1-x8_5)**2).mean())
+print(((x8_1-x8_3)**2).mean())
+print(((x8_3-x8_5)**2).mean())
+print(np.corrcoef([x8_3.ravel(), x8_5.ravel()]))
