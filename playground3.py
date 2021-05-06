5caf683c95ee4e2f73d358b64b19f3ac8cf96b0c
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2021-05-06 16:15:24 -0700

    playground3

--- playground3.py
+++ playground3.py
@@ -24,6 +24,7 @@ model2.fit(epochs=100)
 import numpy as np
 import pandas as pd
 import dask_ml.preprocessing as dmlp
+import dask_ml.decomposition as dmld
 import tensorflow as tf
 import tensorflow.keras as tfk
 from common.defs import lazy_property
@@ -35,12 +36,16 @@ from helpers import chunk_iter
 from ae import Sparse1
 import xarray as xa
 import itertools as it
+import shutil
+import matplotlib.pyplot as plt
 
-class _data:
+
+class _data(ae.data1):
     @property
     def data1(self):
         mat = xa.Dataset()
         mat['data'] = (('rows', 'cols'), daa.from_array(np.random.randn(1000, 500)))
+        return mat
 
     def _split(self, ratio):
         data1 = self.data1.data.data
@@ -89,8 +94,58 @@ class _data:
 
     @lazy_property
     def ij(self):
-        return np.cross(np.arange(2), np.arange(3))
-
         m = self.num_cols
-        ij = np.array(list(it.product(np.arange(10), np.arange(m))))
-        ij = 
+        ij = np.array(list(it.product(np.arange(200), np.arange(m))))
+        ij = pd.DataFrame(ij).rename(columns={0: 'i', 1: 'j'})
+        return ij
+
+    @lazy_property
+    def data(self):
+        data2 = self.data2
+        data2 = SimpleNamespace(
+            train=data2.train.compute(),
+            test=data2.test.compute()
+        )
+        data4 = SimpleNamespace(
+            Xy=[data2.train, data2.train],
+            kwargs=dict(
+                validation_data=(data2.test, data2.test),
+                batch_size=50
+            )
+        )
+        return data4
+
+model1 = ae.model1()
+model1.data = _data()
+model1.data.storage = Dir(storage/'_data')
+model1.storage = Path(model1.data.storage.path)/'model1'
+shutil.rmtree(model1.data.storage.path, ignore_errors=True)
+model1.data.storage.exists
+model1.fit(epochs=1)
+
+model2 = ae.model2()
+model2.data = _data()
+model2.data.storage = Dir(storage/'_data')
+model2.storage = Path(model2.data.storage.path)/'model2'
+model2.fit(epochs=1)
+
+x1_1 = model2.data.data2.train
+x1_2 = model2.data.data2.test
+x2 = dmld.PCA(n_components=200).fit(x1_1)
+x3_1 = x2.inverse_transform(x2.transform(x1_1))
+x3_2 = x2.inverse_transform(x2.transform(x1_2))
+x4_1 = model2.model.predict(x1_1)
+x4_2 = model2.model.predict(x1_2)
+
+print(((x1_1-x3_1)**2).mean().compute())
+print(((x1_2-x3_2)**2).mean().compute())
+
+print(((x1_1-x4_1)**2).mean().compute())
+print(((x1_2-x4_2)**2).mean().compute())
+
+print(((x3_1-x4_1)**2).mean().compute())
+print(((x3_2-x4_2)**2).mean().compute())
+
+
+
+
