dfe2bf588bee90b5087077b71c0365d08fa47b5b
Trifonov, Vladimir [JANUS] <VTrifono@its.jnj.com>
2021-05-11 11:56:05 -0700

    depmap

---
+++ depmap_crispr.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import xarray as xa
+import numpy as np
+from common.defs import lazy_property
+from depmap.depmap import public_21q1 as release
+
+class CRISPR:
+    @property
+    def release(self):
+        return release
+
+
+    @lazy_property
+    def data(self):
+        expr = release.crispr_effect
+
+        rows = expr.iloc[:,0].rename('rows')
+
+        samples = release.samples.copy()
+        samples = samples.rename(columns={'DepMap_ID': 'rows'})
+        samples = samples.query('rows.isin(@rows)')
+
+        cols = pd.Series(expr.columns[1:]).to_frame('cols')
+        cols['symbol'] = cols.cols.str.replace(' .*$', '', regex=True)
+        cols['entrez'] = cols.cols.str.replace('^.*\(|\)$', '', regex=True).astype(int)
+
+        mat = np.array(expr.iloc[:,1:])
+
+        data = xa.Dataset()
+        data['rows'] = ('rows', rows)
+        data = data.merge(samples.set_index('rows').to_xarray())
+        data = data.merge(cols.set_index('cols').to_xarray())
+        data['mat'] = (('rows', 'cols'), mat)
+
+        return data
+
+
+crispr = CRISPR()
