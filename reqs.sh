f44d493170cac38b7d0c9ebb9c2723c67bc4622d
Vladimir Trifonov <v_trifonov@yahoo.com>
2023-04-04 17:13:31 -0700

    env

---
+++ reqs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+conda install python==3.7 ipykernel
+conda install seaborn
+conda install dask==2021.10.0
+conda install xarray
+conda install zarr
+conda install importlib_metadata
+conda install dask-ml
+conda install more-itertools
+conda install requests
+conda install s3fs
+conda install psycopg2
+conda install mysql-connector-python
+conda install sparse
